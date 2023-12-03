f23a4e826e4bddf4baf2ada8c9fd2b5be368ef4e
Anurag Uranw <skanuraguranw12@gmail.com>
2023-12-03 12:27:45 +0530

    Add code

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function workingProperly() {
-    alert("working properly");
-}
+const addBooks = document.querySelector("#add-books");
+const libraryForm = document.querySelector(".lib-form");
+let bgBlur = document.querySelector(".bg");
 
-const btn1 = document.querySelector("#add-books");
+addBooks.addEventListener('click', () => {
+    libraryForm.style.display = "block";
+    bgBlur.setAttribute("class", "bg-blur");
+})
 
-btn1.addEventListener('click', () => {
-    workingProperly();
+const backBtn = document.querySelector("#back");
+backBtn.addEventListener('click', () => {
+    libraryForm.style.display = "none";
+    bgBlur.setAttribute("class", "bg");
 })
